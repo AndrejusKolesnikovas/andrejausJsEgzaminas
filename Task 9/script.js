79993e1131d877e99d3cecb937c05cc103134a43
AndrejusKolesnikovas <andrejuskolesnikovas@yahoo.com>
2022-06-24 15:48:55 +0300

    using construktor. task finish

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,21 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+    constructor(title, director,budget) {
+      this.title = title;
+      this.director = director;
+      this.budget = +budget;
+    }
+    wasExpensive() {
+        if (this.budget>100000000) {
+            return console.log('true')
+        }return console.log('false')
+    }
+    // wasExpensive() {console.log(`${this.title} ir ${this.director}`)}
+  }
+   
+  const movie1 = new Movie('One supone time', 'Andrejus Kolesnikovas',150600);
+  console.log(movie1);
+  movie1.wasExpensive();
