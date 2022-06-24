95cef59c36f80d3bf65df6023d77f0a24a91be50
AndrejusKolesnikovas <andrejuskolesnikovas@yahoo.com>
2022-06-24 16:51:05 +0300

    po prettier

--- Task 9/script.js
+++ Task 9/script.js
@@ -11,19 +11,20 @@ wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada 
 ------------------------------------------------------------------------------------------------------ */
 
 class Movie {
-    constructor(title, director,budget) {
-      this.title = title;
-      this.director = director;
-      this.budget = +budget;
-    };
-    wasExpensive() {
-        if (this.budget>100000000) {
-             console.log('true')
-        } console.log('false')
+  constructor(title, director, budget) {
+    this.title = title;
+    this.director = director;
+    this.budget = +budget;
+  }
+  wasExpensive() {
+    if (this.budget > 100000000) {
+      console.log("true");
     }
-    // wasExpensive() {console.log(`${this.title} ir ${this.director}`)}
-  };
-   
-  const movie1 = new Movie('One supone time', 'Andrejus Kolesnikovas',150600);
-  console.log(movie1);
-  movie1.wasExpensive();
+    console.log("false");
+  }
+  // wasExpensive() {console.log(`${this.title} ir ${this.director}`)}
+}
+
+const movie1 = new Movie("One supone time", "Andrejus Kolesnikovas", 150600);
+console.log(movie1);
+movie1.wasExpensive();
