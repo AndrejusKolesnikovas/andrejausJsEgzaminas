dc1d5f11ab051f978ad2ece225b3a8babfa8870c
AndrejusKolesnikovas <andrejuskolesnikovas@yahoo.com>
2022-06-27 10:41:53 +0300

    showed cars from json

--- Task 4/script.js
+++ Task 4/script.js
@@ -10,3 +10,49 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+const divOutput = document.querySelector('#output');
+
+
+const getUsers = () =>
+  fetch(ENDPOINT).then((response) => {
+    if (!response.ok) {
+      throw new Error('Įvyko klaida: ' + response.status);
+    }
+    return response.json();
+  });
+
+  initDocument();  
+  function initDocument() {
+    getUsers().then((data) => {
+      console.log(data);
+      let promises = data.map((user) =>
+        getUsers(user.brand,user.models).then((brand,models) => {
+          user.brand[0] = brand;
+          console.log(user.models)
+        user.models[0]= models;
+
+          return user;
+        })
+      );
+      Promise.all(promises).then((result) => {
+        users = result;
+        arrangeData();
+      });
+    });
+  
+  };
+  
+  function arrangeData() {
+    let usersElements = '';
+    users.forEach((user) => {
+      if (user) {
+        usersElements += `<tr>
+            <th>${user.brand}</th>
+        </tr>
+        <tr>
+            <td>${user.models}</td>
+        </tr>`;
+        divOutput.innerHTML = usersElements;
+      }
+    });
+  };
