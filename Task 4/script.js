8db4f131c5dd0daa0eaf6db676e9b5c873cd3b62
AndrejusKolesnikovas <andrejuskolesnikovas@yahoo.com>
2022-06-27 13:21:14 +0300

    begin response

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,50 +9,58 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
-const divOutput = document.querySelector('#output');
+const ENDPOINT = "cars.json";
+const divOutput = document.querySelector("#output");
+const table = document.createElement('table');
+divOutput.append(table);
+
+
 
 
 const getUsers = () =>
   fetch(ENDPOINT).then((response) => {
     if (!response.ok) {
-      throw new Error('Įvyko klaida: ' + response.status);
+      throw new Error("Įvyko klaida: " + response.status);
     }
     return response.json();
   });
 
-  initDocument();  
-  function initDocument() {
-    getUsers().then((data) => {
-      console.log(data);
-      let promises = data.map((user) =>
-        getUsers(user.brand,user.models).then((brand,models) => {
-          user.brand[0] = brand;
-          console.log(user.models)
-        user.models[0]= models;
+initDocument();
+function initDocument() {
+  getUsers().then((data) => {
+    console.log(data);
+    let promises = data.map((user) =>
+      getUsers(user.brand, user.models).then((brand, models) => {
+        user.brand[0] = brand;
+        console.log(user.models);
+        user.models[0] = models;
 
-          return user;
-        })
-      );
-      Promise.all(promises).then((result) => {
-        users = result;
-        arrangeData();
-      });
+        return user;
+      })
+    );
+    Promise.all(promises).then((result) => {
+      users = result;
+      arrangeData();
     });
-  
-  };
-  
-  function arrangeData() {
-    let usersElements = '';
-    users.forEach((user) => {
-      if (user) {
-        usersElements += `<tr>
+  });
+}
+
+function arrangeData() {
+  let usersElements = "";
+  users.forEach((user) => {
+    if (user) {
+      usersElements += `
+      
+      <tr>
             <th>${user.brand}</th>
         </tr>
         <tr>
-            <td>${user.models}</td>
-        </tr>`;
-        divOutput.innerHTML = usersElements;
-      }
-    });
-  };
+            <td> 
+               Models:<ul>${user.models.join('<li>*</li>')}</ul>
+            </td>
+        </tr>
+       `;
+       table.innerHTML = usersElements;
+    }
+  });
+}
